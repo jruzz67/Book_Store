a548a69dd946c2dcdd879e2e103840afa13661f9
jruzz67 <j1567ruzz@gmail.com>
2025-03-08 01:15:37 +0530

    Update Book.java

--- springapp/src/main/java/com/examly/springapp/entities/Book.java
+++ springapp/src/main/java/com/examly/springapp/entities/Book.java
@@ -2,33 +2,28 @@ package com.examly.springapp.entities;
 
 import javax.persistence.*;
 import lombok.*;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class Book {
-    
+public class Ordertable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String title;
-    private String author;
-    private String genre;
-    private String description;
-    private double price;
+    private LocalDateTime orderDate;
+    private double totalAmount;
+    private String status;
     @ManyToOne
+    @JoinColumn(name = "user_id")
     private User user;
-    public void setUser(User user) {
-        this.user=user;
-    }
-    public User getUser() {
-        return this.user;
-    }
-    public String getTitle(){
-        return this.title;
-    }
-    public String getAuthor(){
-        return this.author;
-    }                                
+    @ManyToMany
+    @JoinTable(
+        name = "order_books",
+        joinColumns = @JoinColumn(name = "order_id"),
+        inverseJoinColumns = @JoinColumn(name = "book_id")
+    )
+    private List<Book> books;
 }
