20a2cd7f472bbff9deb35eb5ac7eceb96daff39a
jruzz67 <j1567ruzz@gmail.com>
2025-03-08 01:16:43 +0530

    Update BookRepository.java

--- springapp/src/main/java/com/examly/springapp/repositories/BookRepository.java
+++ springapp/src/main/java/com/examly/springapp/repositories/BookRepository.java
@@ -2,9 +2,13 @@ package com.examly.springapp.repositories;
 
 import com.examly.springapp.entities.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
     List<Book> findByGenre(String genre);
+     @Query("SELECT b FROM Book b WHERE b.title = :title")
+         List<Book> findBooksByTitle(@Param("title") String title);
 }
