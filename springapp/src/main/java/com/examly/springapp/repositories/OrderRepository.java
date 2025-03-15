76595efdd081ad118d919a3105f1cfa48859fc22
jruzz67 <j1567ruzz@gmail.com>
2025-03-15 12:03:36 +0530

    Update OrderRepository.java

--- springapp/src/main/java/com/examly/springapp/repositories/OrderRepository.java
+++ springapp/src/main/java/com/examly/springapp/repositories/OrderRepository.java
@@ -1,10 +1,38 @@
-package com.examly.springapp.repositories;
+package com.examly.springapp.services;
 
 import com.examly.springapp.entities.Ordertable;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import com.examly.springapp.repositories.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-@Repository
-public interface OrderRepository extends JpaRepository<Ordertable, Long> {
+import java.util.List;
+import java.util.Optional;
 
+@Service
+public class OrderService {
+    @Autowired
+    private OrderRepository orderRepository;
+    public Ordertable createOrder(Ordertable order) {
+        return orderRepository.save(order);
+    }
+    public Optional<Ordertable> getOrderById(Long id) {
+        return orderRepository.findById(id);
+    }
+    public List<Ordertable> getAllOrders() {
+        return orderRepository.findAll();
+    }
+    public Ordertable updateOrder(Long id, Ordertable orderDetails) {
+        Optional<Ordertable> existingOrder = orderRepository.findById(id);
+        if (existingOrder.isPresent()) {
+            Ordertable order = existingOrder.get();
+            order.setOrderDate(orderDetails.getOrderDate());
+            order.setTotalAmount(orderDetails.getTotalAmount());
+            order.setStatus(orderDetails.getStatus());
+            return orderRepository.save(order);
+        }
+        return null;
+    }
+    public void deleteOrder(Long id) {
+        orderRepository.deleteById(id);
+    }
 }
